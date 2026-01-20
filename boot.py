ffc6d74595bf0b50bee41ecfe9a36a8070ea7020
lain <lolc7089@gmail.com>
2026-01-20 18:44:36 +0400

    test1

---
+++ boot.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+def start_boot():
+    os.system("figlet -f slant 'gemboot'")
+    print("[-] Waiting for iPhone 6 in DFU mode...")
+    os.system("./everything/ipwndfu -p") 
+    time.sleep(5)
+    print("Waiting...")
+    os.system("./everything/ipwndfu --load ./pongoOS.bin")
+
+if __name__ == "__main__":
+    start_boot()
